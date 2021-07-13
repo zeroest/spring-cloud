a9696f39241d068eaa7417024ffd60420182bd52
kyd <husheart@naver.com>
2021-07-14 02:48:22 +0900

    gateway load balancer

--- first-service/src/main/java/me/zeroest/firstservice/WelcomController.java
+++ first-service/src/main/java/me/zeroest/firstservice/WelcomController.java
@@ -1,15 +1,22 @@
 package me.zeroest.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/first-service")
 public class WelcomController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,8 +30,9 @@ public class WelcomController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "check first";
+    public String check(HttpServletRequest request) {
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("Second on PORT %s",  env.getProperty("local.server.port"));
     }
 
 }
