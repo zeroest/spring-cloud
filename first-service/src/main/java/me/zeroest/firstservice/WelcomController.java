8b7b9d1e4509482d9800f6b136340de08e72c8a4
kyd <husheart@naver.com>
2021-07-14 01:10:08 +0900

    gateway filter in java code

--- first-service/src/main/java/me/zeroest/firstservice/WelcomController.java
+++ first-service/src/main/java/me/zeroest/firstservice/WelcomController.java
@@ -1,9 +1,12 @@
 package me.zeroest.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class WelcomController {
@@ -11,6 +14,12 @@ public class WelcomController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome first service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return header;
     }
 
 }
--- second-service/src/main/java/me/zeroest/secondservice/WelcomController.java
+++ second-service/src/main/java/me/zeroest/secondservice/WelcomController.java
@@ -1,9 +1,12 @@
 package me.zeroest.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class WelcomController {
@@ -11,6 +14,12 @@ public class WelcomController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return header;
     }
 
 }
