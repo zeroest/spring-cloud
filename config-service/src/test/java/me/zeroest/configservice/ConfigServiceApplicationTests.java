5da152d3ef58ae418a7f151edcf58c09b1ba19d8
kyd <husheart@naver.com>
2021-07-22 01:50:51 +0900

    Config - production setup

--- config-service/src/test/java/me/zeroest/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/me/zeroest/configservice/ConfigServiceApplicationTests.java
@@ -3,7 +3,7 @@ package me.zeroest.configservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ConfigServiceApplicationTests {
 
     @Test
