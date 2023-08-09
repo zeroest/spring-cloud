7cdc6b1fd2c7055ca2590f252bc22385bbeb95ae
zeroest <zeroest.me@gmail.com>
2023-08-10 02:52:01 +0900

    Gateway version up

--- gateway/src/main/java/me/zeroest/gateway/config/HttpTraceConfig.java
+++ gateway/src/main/java/me/zeroest/gateway/config/HttpTraceConfig.java
@@ -1,7 +1,14 @@
 package me.zeroest.gateway.config;
 
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+/*
+* https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.0-Migration-Guide#httptrace-endpoint-renamed-to-httpexchanges
+* Related infrastructure classes have also been renamed. For example, HttpTraceRepository is now named HttpExchangeRepository and can be found in the org.springframework.boot.actuate.web.exchanges package.
+* */
+
+//import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+//import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +16,8 @@ import org.springframework.context.annotation.Configuration;
 public class HttpTraceConfig {
 
     @Bean
-    public HttpTraceRepository httpTraceRepository() {
-        return new InMemoryHttpTraceRepository();
+    public HttpExchangeRepository httpTraceRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
